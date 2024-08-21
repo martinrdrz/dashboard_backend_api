21c46be8e13ceb8c527bf5a2e0e7846a11e610ab
martinrdrz <martinrdrz@hotmail.com>
2024-08-21 11:59:06 -0300

    primer commit

---
+++ database/config.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.DB_CNN);
+        //El siguiente objeto es el segndo parametro del metodo connect anterior, pero en las versiones nuevas da error, por eso se eliminó
+        // {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        // }
+
+        console.log('DB Online');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de inicializar BD');
+    }
+};
+
+module.exports = {
+    dbConnection,
+};
---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+// Directorio Público
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
+//Este caso es apra cuando ponemos el frontend en la carpeta public del backend y queremos acceder a alguna ruta de manera directa, como cuando teniamos el frontend separado, por ejmplo "host/auth/login" y dicha ruta va a ser interceptada por el express y no la tiene definida, por tal motivo la va a atrapar este nueva linea y le da curso correcto.
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
