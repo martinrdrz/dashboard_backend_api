c5f122bec18c73b54280814cfb9462922fd4e7f2
martinrdrz <martinrdrz@hotmail.com>
2024-08-27 19:16:15 -0300

    agregado de comentarios

--- routes/sistemas.js
+++ routes/sistemas.js
@@ -5,7 +5,9 @@ const { getSistemas, getSistema } = require('../controllers/sistemas');
 
 const router = Router();
 
+//Devuelve todos los documentos/sistemas de la base de datos
 router.get('/', getSistemas);
+//Devuelve solo el documento/siste indicado con el email
 router.get('/:email', [check('email', 'El email es obligatorio').isEmail(), validarCampos], getSistema);
 
 module.exports = router;
