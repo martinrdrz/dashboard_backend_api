4c35f6db060770ef7a5e09e8cff218e303de209a
martinrdrz <martinrdrz@hotmail.com>
2024-08-28 19:37:33 -0300

    aa

--- services/sistemas.js
+++ services/sistemas.js
@@ -33,7 +33,41 @@ const getData = async (email) => {
         //todo
         let systemsData = {};
 
-        // en systemsData se almacena en formato json cada uno de los dvalores de los datos de cada sistema para el usuario dado
+        //En systemsData se almacena en formato json cada uno de los valores de los datos de cada sistema para el usuario dado
+        for (let i = 1; i <= userSystemsData.cant_sistemas; i++) {
+            const systemKey = `sistema_${i}`;
+            const system = userSystemsData[systemKey];
+            if (!system) {
+                throw new Error('Uno de los sistemas no posee datos para leer.');
+            }
+            systemsData[systemKey] = {};
+
+            for (let j = 1; j <= userSystemsData[systemKey].cant_datos; j++) {
+                const dataKey = `dato_${j}`;
+                const dataValue = userSystemsData[systemKey][dataKey];
+                if (!dataValue) {
+                    throw new Error('No existe el dato data_${j} del sistema sistema_${i}.');
+                }
+                systemsData[systemKey][dataKey] = dataValue;
+            }
+        }
+        console.log(systemsData);
+        return systemsData;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const getData_OK = async (email) => {
+    try {
+        const userSystemsData = await Sistema.findOne({ email });
+        if (!userSystemsData) {
+            throw new Error('No existe un sistema para ese email');
+        }
+        //todo
+        let systemsData = {};
+
+        //En systemsData se almacena en formato json cada uno de los valores de los datos de cada sistema para el usuario dado
         for (let i = 1; i <= userSystemsData.cant_sistemas; i++) {
             const systemKey = `sistema_${i}`;
             const system = userSystemsData[systemKey];
